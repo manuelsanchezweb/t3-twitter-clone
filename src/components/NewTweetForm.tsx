26e9bf211e71ed4d925710eaf1d3ad852fa04c47
manuelsanchez2 <manusansan22@gmail.com>
2023-05-10 15:40:07 +0200

    🆕 feat: Make tweets come without refresh

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -27,14 +27,44 @@ function Form() {
     textAreaRef.current = textArea;
   }, []);
 
+  const trpcUtils = api.useContext();
+
   useLayoutEffect(() => {
     updateTextAreaSize(textAreaRef.current);
   }, [inputValue]);
 
   const createTweet = api.tweet.create.useMutation({
-    onSuccess: () => {
+    onSuccess: (newTweet) => {
       //   console.log(newTweet);
       setInputValue("");
+
+      if (session.status !== "authenticated") return;
+
+      trpcUtils.tweet.infiniteFeed.setInfiniteData({}, (oldData) => {
+        if (oldData == null || oldData.pages[0] == null) return oldData;
+
+        const newCachedTweet = {
+          ...newTweet,
+          likeCount: 0,
+          likedByMe: false,
+          user: {
+            id: session.data?.user.id,
+            name: session.data?.user.name || null,
+            image: session.data?.user.image || null,
+          },
+        };
+
+        return {
+          ...oldData,
+          pages: [
+            {
+              ...oldData.pages[0],
+              tweets: [newCachedTweet, ...oldData.pages[0].tweets],
+            },
+            ...oldData.pages.slice(1),
+          ],
+        };
+      });
     },
   });
 
